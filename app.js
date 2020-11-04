473ce6733a4016ed4bdf7e166f6c1d8b1c2df228
pablolujambio <lpablo1983@gmail.com>
2020-11-04 11:05:02 +0100

    cree NUEVO productCart.html (el carrito), MODIFIQUE home.css para el carrito solamente y app.js solo para escucha

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.get('/detalle', function(req, res){
     res.sendFile(path.join(__dirname, "./views/productDetail.html"))
 })
 
+app.get('/carrito', function(req, res){
+    res.sendFile(path.join(__dirname, "./views/productCart.html"))
+})
+
 app.listen(3000, function() {
     console.log('servidor esta funcionando')
 })
