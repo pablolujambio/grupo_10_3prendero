3dc0506b37be117b01ccb12cd0b3a98e5fe50b99
pablolujambio <lpablo1983@gmail.com>
2020-11-21 19:35:47 +0100

    separar carpetas

--- app.js
+++ app.js
@@ -6,24 +6,25 @@ app.use( express.static(path.join(__dirname, "./public")))
 
 
 app.get('/home', function(req, res){
-    res.sendFile(path.join(__dirname, "./views/index.html"))
+    res.sendFile(path.join(__dirname, ".", "src", "views", "index.html"))
 })
 
 app.get('/detalle', function(req, res){
-    res.sendFile(path.join(__dirname, "./views/productDetail.html"))
+    res.sendFile(path.join(__dirname, ".", "src", "views", "products", "productDetail.html"))
 })
 
 app.get('/carrito', function(req, res){
-    res.sendFile(path.join(__dirname, "./views/productCart.html"))
+    res.sendFile(path.join(__dirname, ".", "src", "views", "products", "productCart.html"))
 })
 
 app.get('/login', function(req, res){
-    res.sendFile(path.join(__dirname, "./views/login.html"))
+    res.sendFile(path.join(__dirname, ".", "src", "views", "users", "login.html"))
 })
 
 app.get('/register', function(req, res){
-    res.sendFile(path.join(__dirname, "./views/register.html"))
+    res.sendFile(path.join(__dirname, ".", "src", "views", "users", "register.html"))
 })
+
 app.listen(3000, function() {
     console.log('servidor esta funcionando')
 })
