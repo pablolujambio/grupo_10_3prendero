47a5e0a722990bb362e8a9cb558f97888dfc7bcc
pablolujambio <lpablo1983@gmail.com>
2020-11-24 15:07:47 +0100

    instalación ejs

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ const app= express();
 const path = require('path')
 
 
-
-
-
-const mainRouter = require('./routes/main');
+const mainRouter = require('src/views/index.ejs');
 const usersRouter = require('./routes/users');
 const productsRouter = require('./routes/products');
 
@@ -20,14 +17,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', mainRouter);
+app.use('/home',mainRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
-
-
-
-
-
 
 
 
