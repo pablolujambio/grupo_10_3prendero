6e7a1882a6ddcfd268262b56176b084c6971f5c9
emilio <emiliozuccarelli51@gmail.com>
2021-02-01 16:24:24 -0300

    confirmar contraseña y email valido

--- src/validations/registervalidator.js
+++ src/validations/registervalidator.js
@@ -1,16 +1,32 @@
 const {check, validationResult, body} = require("express-validator");
+let db = require('../database/models');
 
 module.exports = [
     check("username")
         .isLength({min:2, max:20}) 
         .withMessage("debe tener como minimo 2 caracteres y como maximo 20"),
+        
     check("email")
-        .isEmail() 
-        .withMessage("debe ingresar email valido"),
+        .notEmpty().withMessage('El campo no puede estar vacío')
+        .isEmail().withMessage("Debes ingresar email válido")
+        .custom(async function(email) {
+            let registro = await db.usuarios.findOne({ where : { email: email } });
+            if (registro) {
+                throw new Error ("El email ya está registrado")
+            }
+        }),
+   
     check("password")
         .isLength({min:6, max:15}) 
         .withMessage("la contraseña debe tener un minimo de 6 caracteres y un maximo de 15 caracteres"),
     check("repassword")
-        .isLength({min:6, max:15}) 
-        .withMessage("la contraseña debe tener un minimo de 6 caracteres y un maximo de 15 caracteres"),
+        .notEmpty().withMessage('El campo no puede estar vacío')
+        .isLength({ min:6, max:15 }).withMessage("la contraseña debe tener un minimo de 6 caracteres y un maximo de 15 caracteres")
+        .custom(async (repassword, {req}) => { 
+            let password = req.body.password;
+            if(password !== repassword){ 
+              throw new Error('Las contraseñas deben ser las mismas') 
+            }
+        }), 
+  
  ]
