b3b7407842259ae23f095597a46762e0d78b6dbb
emilio <emiliozuccarelli51@gmail.com>
2021-03-04 12:52:35 -0300

    dashboard de productos, categorías y usuarios estatico en react

--- 3-prendero/src/components/Main/index.js
+++ 3-prendero/src/components/Main/index.js
@@ -3,6 +3,69 @@ import React, {Component} from 'react'
 const  Main = () =>  {
     return ( 
         <main>
+       <div class="lineaarriba">
+     
+						<div class="caja">
+                        <div class="contiene">
+								<div class="body">
+									
+										<div class="izq">
+                                            <div class="uno">
+											<p> Productos en base de datos</p>
+                                            </div>
+                                            <div class="dos">
+											<p> 135</p>
+                                            </div>
+										</div>
+										<div class="der">
+											<i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+										</div>
+									</div>
+								</div>
+                         </div>
+
+                         <div class="caja">
+                        <div class="contiene">
+								<div class="body">
+									
+										<div class="izq">
+                                            <div class="uno">
+											<p> Categorias en base de datos</p>
+                                            </div>
+                                            <div class="dos">
+											<p> 135</p>
+                                            </div>
+										</div>
+										<div class="der">
+											<i class="fas fa-bars fa-2x text-gray-300"></i>
+										</div>
+									</div>
+								</div>
+                         </div>
+                          
+
+                         <div class="caja">
+                        <div class="contiene">
+								<div class="body">
+									
+										<div class="izq">
+                                            <div class="uno">
+											<p> Usuarios en base de datos</p>
+                                            </div>
+                                            <div class="dos">
+											<p> 135</p>
+                                            </div>
+										</div>
+										<div class="der">
+											<i class="fas fa-user fa-2x text-gray-300"></i>
+										</div>
+									</div>
+								</div>
+                         </div>
+                          
+                          
+							
+						</div>
        
         </main>
  )
