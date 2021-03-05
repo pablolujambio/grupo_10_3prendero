a8fc2c013935ca3afd20154db0bfc7a72c1f90d1
emilio <emiliozuccarelli51@gmail.com>
2021-03-05 15:56:05 -0300

    diseño de el ultimo producto en la base y categorias en la base con css y media querie

--- 3-prendero/src/components/Main/index.js
+++ 3-prendero/src/components/Main/index.js
@@ -1,63 +1,64 @@
 import React, {Component} from 'react'
+import Remera from './images/Remera.jpg';
 
 const  Main = () =>  {
     return ( 
         <main>
-       <div class="lineaarriba">
+       <div className="lineaarriba">
      
-						<div class="caja">
-                        <div class="contiene">
-								<div class="body">
+						<div className="caja">
+                        <div className="contiene">
+								<div className="body">
 									
-										<div class="izq">
-                                            <div class="uno">
+										<div className="izq">
+                                            <div className="uno">
 											<p> Productos en base de datos</p>
                                             </div>
-                                            <div class="dos">
+                                            <div className="dos">
 											<p> 135</p>
                                             </div>
 										</div>
-										<div class="der">
-											<i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+										<div className="der">
+											<i className="fas fa-clipboard-list fa-2x text-gray-300"></i>
 										</div>
 									</div>
 								</div>
                          </div>
 
-                         <div class="caja">
-                        <div class="contiene">
-								<div class="body">
+                         <div className="caja">
+                        <div className="contiene">
+								<div className="body">
 									
-										<div class="izq">
-                                            <div class="uno">
+										<div className="izq">
+                                            <div className="uno">
 											<p> Categorias en base de datos</p>
                                             </div>
-                                            <div class="dos">
+                                            <div className="dos">
 											<p> 135</p>
                                             </div>
 										</div>
-										<div class="der">
-											<i class="fas fa-bars fa-2x text-gray-300"></i>
+										<div className="der">
+											<i className="fas fa-bars fa-2x text-gray-300"></i>
 										</div>
 									</div>
 								</div>
                          </div>
                           
 
-                         <div class="caja">
-                        <div class="contiene">
-								<div class="body">
+                         <div className="caja">
+                        <div className="contiene">
+								<div className="body">
 									
-										<div class="izq">
-                                            <div class="uno">
+										<div className="izq">
+                                            <div className="uno">
 											<p> Usuarios en base de datos</p>
                                             </div>
-                                            <div class="dos">
+                                            <div className="dos">
 											<p> 135</p>
                                             </div>
 										</div>
-										<div class="der">
-											<i class="fas fa-user fa-2x text-gray-300"></i>
+										<div className="der">
+											<i className="fas fa-user fa-2x text-gray-300"></i>
 										</div>
 									</div>
 								</div>
@@ -66,6 +67,103 @@ const  Main = () =>  {
                           
 							
 						</div>
+
+                        <div class="centro">
+					
+						<div class="caja">
+							
+								<div class="arriba">
+                                    <div class= "titulo">
+									<h6>Ultimo producto en la base de datos</h6>
+                                    </div>
+								</div>
+								<div class="abajo">
+									<div class="imagen">
+										<img src={Remera} alt="image" />
+									</div>
+									<div class= "nombre">
+                                        <p>Remera blanca</p>
+                                    </div>
+                                    <div class= "precio">
+                                     <p>$800</p>
+                                     </div>
+                                     <div class= "descripcion">
+                                     <p>Remera blanca lisa para hombre ultima moda</p>
+                                     </div>
+
+								</div>
+							
+						</div>
+
+                        <div class="caja">
+							
+								<div class="arriba">
+                                    <div class= "titulo2">
+									<h6>Categorias en base de datos</h6>
+                                    </div>
+								</div>
+								<div class="abajo">
+                               
+                                <div class="cajas">
+										
+												<div class="caja">
+                                                <div class="categoria">
+                                                <p>Category 01</p>
+                                                </div>
+												</div>
+											
+										
+										
+										
+												<div class="caja">
+                                                <div class="categoria">
+                                                <p>Category 02</p>
+                                                </div>
+												</div>
+											
+										
+										
+										
+												<div class="caja">
+                                                <div class="categoria">
+                                                <p>Category 03</p>
+                                                </div>
+												</div>
+										
+									
+									
+										
+												<div class="caja">
+                                                <div class="categoria">
+                                                <p>Category 04</p>
+                                                </div>
+												</div>
+										
+									
+										
+										
+												<div class="caja">
+                                                    <div class="categoria">
+                                                <p>Category 05</p>
+                                                </div>
+												</div>
+											
+										
+										
+										
+												<div class="caja">
+                                                <div class="categoria">
+                                                <p>Category 06</p>
+                                                </div>
+												</div>
+										
+										
+							
+
+								</div>
+                                </div>
+						</div>
+                        </div>
        
         </main>
  )
